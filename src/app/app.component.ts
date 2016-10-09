e764b655267064923762589542b61dcbf7a494f2
Aleksandar Djindjic <djindjic@gmail.com>
2016-10-09 18:24:13 +0200

    dist
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,15 +41,6 @@ import { AppState } from './app.service';
     </main>
 
     <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-    <footer>
-      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
-      <div>
-        <a [href]="url">
-          <img [src]="angularclassLogo" width="25%">
-        </a>
-      </div>
-    </footer>
   `
 })
 export class App {
